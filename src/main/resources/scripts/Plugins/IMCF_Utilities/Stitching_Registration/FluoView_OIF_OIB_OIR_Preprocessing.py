25244713c244c9111f66bff7a622a907dd3b8ab7
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-01-16 18:32:21 +0100

    Allow specifying the output directory
    This is useful if the input data is e.g. on a network share and the
    produced files (intermediate as well as stitchings) should be placed on
    local storage for performance reasons.

--- src/main/resources/scripts/Plugins/IMCF_Utilities/Stitching_Registration/FluoView_OIF_OIB_OIR_Preprocessing.py
+++ src/main/resources/scripts/Plugins/IMCF_Utilities/Stitching_Registration/FluoView_OIF_OIB_OIR_Preprocessing.py
@@ -1,6 +1,7 @@
 #@ String(visibility=MESSAGE,persist=false,label="<html><div align='center'><h2>Pre-processing of<br>Olympus FluoView mosaics<br><br>OIF / OIB / OIR</h2></div></html>",value="<html><img src='http://imagej.net/_images/5/5e/Tiles-Overlay.png'></html>") msg_header
 #@ File(label="<html><div align='left'><h3>Supported input files</h3>&bull; [ <tt>MATL_Mosaic.log</tt> ]<br/>&bull; [ <tt>matl.omp2info</tt> ]</div></html>",description="[ MATL_Mosaic.log ] or [ matl.omp2info ] file") infile
 #@ File(label="Shading correction model",description="single slice, single channel TIFF file",style="extensions:tif/tiff") model_file
+#@ File(label="Output directory",description="location for results and intermediate processing files, use 'NONE' for input dir",style="directory", value="NONE", persist=false) out_dir
 #@ Boolean(label="Show generated code",description="Print generated code to log messages for debugging") print_code
 
 #@ String(visibility=MESSAGE,label="<html><br/><h3>Citation note</h3></html>",value="<html><br/>Stitching is based on a publication, if you're using it for your research please <br>be so kind to cite it:<br><a href=''>Preibisch et al., Bioinformatics (2009)</a></html>",persist=false) msg_citation
@@ -75,8 +76,14 @@ if len(mosaics) == 0:
     exit("Couldn't find any (valid) mosaics in the project file!")
 log.info(mosaics.summarize())
 
+outdir = str(out_dir)
+if outdir == "-" or outdir == "NONE":
+    outdir = indir
+    log.info("No output directory given, using input directory [%s]." % outdir)
+else:
+    log.info("Using output directory [%s] for results and temp files." % outdir)
+
 log.info("Pre-processing stacks: shading correction and projections...")
-outdir = join(indir, 'preprocessed')
 shading.process_folder(indir, 'oir', outdir, str(model_file), '.ics')
 
 log.info('Writing tile configuration files.')
