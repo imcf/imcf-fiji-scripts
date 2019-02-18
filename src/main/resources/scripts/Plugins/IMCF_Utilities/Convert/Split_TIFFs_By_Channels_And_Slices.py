3eb419f899714a8b7c31bbfdb9387486bcce4f2b
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-02-18 16:52:53 +0100

    Use split_by_c_and_z() from imcflibs.imagej.split

--- src/main/resources/scripts/Plugins/IMCF_Utilities/Convert/Split_TIFFs_By_Channels_And_Slices.py
+++ src/main/resources/scripts/Plugins/IMCF_Utilities/Convert/Split_TIFFs_By_Channels_And_Slices.py
@@ -8,14 +8,11 @@
 #@ File(label="Input Directory",style="directory") src_dir
 #@ Integer(label="Skip number of slices at top",value=0) skip_top
 #@ Integer(label="Skip number of slices at bottom",value=0) skip_bottom
-
-from ij import IJ, ImagePlus
-from ij.io import FileSaver
-from ij.plugin import ChannelSplitter
 #@ LogService sjlogservice
 
 import os
 
+from imcflibs.imagej.split import split_by_c_and_z
 
 # type checks and explicit pylint disabling for scijava parameters
 src_dir = str(src_dir)  # pylint: disable-msg=E0601
@@ -23,23 +20,6 @@ skip_top = int(skip_top)   # pylint: disable-msg=E0601
 skip_bottom = int(skip_bottom)   # pylint: disable-msg=E0601
 log = sjlogservice  # pylint: disable-msg=E0602
 
-def split_by_c_and_z(dname, imgf, skip_top, skip_bottom):
-    log.info("Processing file [%s]" % imgf)
-    imp = IJ.openImage(dname + "/" + imgf)
-    fname = os.path.splitext(imgf)
-    channels = ChannelSplitter().split(imp)
-    for channel in channels:
-        ch_name = channel.getTitle().split("-")[0]
-        tgt_dir = os.path.join(dname, fname[0] + "-" + ch_name)
-        if not os.path.isdir(tgt_dir):
-            os.mkdir(tgt_dir)
-        stack = channel.getStack()
-        for z in range(1+skip_top, stack.getSize()+1-skip_bottom):
-            ip = stack.getProcessor(z)
-            fout = "%s/%s-z%s%s" % (tgt_dir, fname[0], z, fname[1])
-            # fout = dname + "/" + ch_name + "/" + fname[0] + "-z" + z + fname[1]
-            log.info("Writing channel %s, slice %s: %s" % (ch_name, z, fout))
-            FileSaver(ImagePlus(fname[0], ip)).saveAsTiff(fout)
 
 for infile in os.listdir(src_dir):
     log.info("Processing directory [%s]" % src_dir)
