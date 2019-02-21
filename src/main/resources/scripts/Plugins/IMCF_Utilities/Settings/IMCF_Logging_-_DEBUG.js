3aca6e20703ee6bdc883300489aadb4c1f995502
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2019-02-21 10:41:17 +0100

    Use the SciJava LogService for logging

--- src/main/resources/scripts/Plugins/IMCF_Utilities/Settings/IMCF_Logging_-_DEBUG.js
+++ src/main/resources/scripts/Plugins/IMCF_Utilities/Settings/IMCF_Logging_-_DEBUG.js
@@ -1,4 +1,8 @@
+//@ LogService log
+
 importClass(Packages.ij.Prefs);
 
 Prefs.set("imcf.debugging", true);
 Prefs.savePreferences();
+
+log.info("Enabled IMCF debug logging.");
--- src/main/resources/scripts/Plugins/IMCF_Utilities/Settings/IMCF_Logging_-_Default.js
+++ src/main/resources/scripts/Plugins/IMCF_Utilities/Settings/IMCF_Logging_-_Default.js
@@ -1,4 +1,8 @@
+//@ LogService log
+
 importClass(Packages.ij.Prefs);
 
 Prefs.set("imcf.debugging", false);
 Prefs.savePreferences();
+
+log.info("Disabled IMCF debug logging.");
